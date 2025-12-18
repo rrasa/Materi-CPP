45604bec757b3a0be0aefd450670f77f433df961
Muhammad Rasya Samrid Pratama <123047396+rrasa@users.noreply.github.com>
2025-12-18 23:39:15 +0700

    Create Input-Output-Tipe_Data.cpp
    Add basic input-output example in C++

---
+++ Input-Output-Tipe_Data.cpp
@@ -0,0 +1,17 @@
+#include <iostream> //dibutuhkan sebagai library utama C++
+using namespace std; //dibutuhkann untuk mempermudah user dalam memahami program
+
+int main () {
+	//cout digunakan untuk menampilkan sebuah program di dalam terminal
+	//dan cout itu ditandai dengan "<<"
+	cout << "Halo Dunia" << endl; //endl digunakan untuk mengenter suatu bari atau kode
+	
+	//tipeData namaVariabel = value, example: string Nama = "Rasya"
+	//tipeData namaVariabel
+	string nama;
+	cout << "Masukkan Nama = ";
+  cin >> nama;
+  
+	
+	
+}
